75ef5191ff53e8c97b8c7e62fabcecc5158d239b
zaenalashter <Santrialit77@gmail.com>
2022-05-05 13:19:33 +0700

    read data

--- mobile_app/lib/screens/homepage.dart
+++ mobile_app/lib/screens/homepage.dart
@@ -1,9 +1,20 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
-  const HomePage({
+  HomePage({
     Key? key,
   }) : super(key: key);
+
+  final String url = 'http://127.0.0.1:8000/api/products';
+  //android emulator:http://10.0.2.2
+
+  Future getProducts() async {
+    var response = await http.get(Uri.parse(url));
+    print(json.decode(response.body));
+    return json.decode(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +22,73 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Jaenal Store'),
       ),
-      body: Container(
-        child: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 30),
-          ),
-        ),
+      body: FutureBuilder(
+        future: getProducts(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: (snapshot.data as dynamic)['data'].length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 180,
+                    child: Card(
+                      elevation: 7,
+                      child: Row(children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          padding: EdgeInsets.all(5),
+                          height: 120,
+                          width: 120,
+                          child: Image.network(
+                            (snapshot.data as dynamic)['data'][index]
+                                ['image_url'],
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    (snapshot.data as dynamic)['data'][index]['name'],
+                                    style: TextStyle(
+                                        fontSize: 20, fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text((snapshot.data as dynamic)['data'][index]
+                                      ['description']),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Icon(Icons.edit),
+                                    Text((snapshot.data as dynamic)['data'][index]
+                                        ['price']),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ]),
+                    ),
+                  );
+                });
+          } else {
+            return Container(
+              child: Center(
+                child: Text('Data Error...'),
+              ),
+            );
+          }
+        },
       ),
     );
   }
