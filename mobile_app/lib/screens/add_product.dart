774ae09b66eefaace589462e4f44ac082922c194
zaenalashter <Santrialit77@gmail.com>
2022-05-07 08:29:49 +0700

    update

--- mobile_app/lib/screens/add_product.dart
+++ mobile_app/lib/screens/add_product.dart
@@ -15,7 +15,8 @@ class AddProduct extends StatelessWidget {
   //Web emulator
     // http.post(Uri.parse('http://127.0.0.1:8000/api/products'));
   //Android emulator
-    final response =  await http.post(Uri.parse('http://10.0.2.2:8000/api/products'),body:{
+    final response =  
+      await http.post(Uri.parse('http://10.0.2.2:8000/api/products'),body:{
       'name': _nameController.text,
       'description': _descriptionController.text,
       'price': _priceController.text,
